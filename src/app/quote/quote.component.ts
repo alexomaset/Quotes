45ba59e2366b6f169d03e1a4b8fc8fd17f4e3c97
alexomaset <alexomaset711@gmail.com>
2019-03-25 19:45:01 +0300

    add initial changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import {Quote} from '../quote';
 export class quoteComponent implements OnInit {
 
   quote = [
-    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to produce our own sunshine energy','rtyy'),
+    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to produce our own sunshine energy',''),
     new Quote(2,'Other times we come downstairs feeling sunny already. Those are great days!','ssst'),
     new Quote(3, 'You only live once, but if you do it right, once is enough','ssst'),
     new Quote(4, 'Either way, I think we can all do with a bit more spark to help us perform miracles and live in joy.','ssst'),
