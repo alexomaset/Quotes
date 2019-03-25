4e592d601f6be6bb73a2c4e9125182abcdf535ee
alexomaset <alexomaset711@gmail.com>
2019-03-25 17:37:32 +0300

    add initial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { quoteComponent } from './quote/quote.component';
 import { QuotesDetailsComponent } from './quote-details/quote-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     quoteComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
