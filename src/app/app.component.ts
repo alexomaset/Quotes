8ff86871115b860c3a627c2b5a1ba2c13570fe5c
alexomaset <alexomaset711@gmail.com>
2019-03-25 08:05:43 +0300

    add quotes class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Quotes} from './quote'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes = [
+      new Quotes(1,''),
+      new Quotes(2,''),
+      new Quotes(3, ''),
+      new Quotes(4, ''),
+      new Quotes(5, ''),
+      new Quotes(6, ''),
+
+  ]
 }
