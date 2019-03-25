3edf26af8911ad37ba63de1d3152b5a62acc23b1
alexomaset <alexomaset711@gmail.com>
2019-03-25 09:50:39 +0300

    add initial changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quotes'
+import {Quotes} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -9,9 +9,9 @@ import {Quote} from '../quotes'
 export class QuoteComponent implements OnInit {
 
   quote = [
-    new Quote(1,'Sometime we can feel a bit dull in the morning and we need to produce our own sunshine energy'),
-    new Quote(2,'Other times we come downstairs feeling sunny already. Those are great days!'),
-    new Quote(3, 'You only live once, but if you do it right, once is enough'),
+    new Quotes(1,'Sometime we can feel a bit dull in the morning and we need to produce our own sunshine energy'),
+    new Quotes(2,'Other times we come downstairs feeling sunny already. Those are great days!'),
+    new Quotes(3, 'You only live once, but if you do it right, once is enough'),
     new Quote(4, 'Either way, I think we can all do with a bit more spark to help us perform miracles and live in joy.'),
     new Quote(5, 'Dont cry because its over, smile because it happened.'),
     new Quote(6, 'Be yourself; everyone else is already taken.'),
